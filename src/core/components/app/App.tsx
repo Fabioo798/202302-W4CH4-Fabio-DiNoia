ff31c0628b3b118cd45bc61384e4716b466fdd5c
Fabio <fabiodn.798@gmail.com>
2023-02-10 08:26:45 +0100

    Add app files

---
+++ src/core/components/app/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css';
+import { Home } from '../../../features/home/page/home';
+
+
+function App() {
+  return (
+    <div className="Container">
+      <Home></Home>
+    </div>
+  );
+}
+
+export default App;
